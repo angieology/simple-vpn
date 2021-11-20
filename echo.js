98adcd7e48acff17f3d388f3baa9238854580eb4
angieology <angelalee.one@gmail.com>
2021-11-19 21:47:49 -0500

    update cipheriv

--- echo.js
+++ echo.js
@@ -1,6 +1,7 @@
-var net = require('net')
-net.createServer(function (stream) {
-  // input and output streams are functioning completely separately and
-  // doesn't create infinte loop.
-  stream.pipe(stream)
-}).listen(5000)
+var net = require("net");
+
+net
+  .createServer(function (stream) {
+    stream.pipe(stream);
+  })
+  .listen(5000);
--- vpn.js
+++ vpn.js
@@ -1,11 +1,31 @@
-var net = require('net')
-var crypto = require('crypto')
-var pw = 'password123'
+var net = require("net");
+var crypto = require("crypto");
+const { PassThrough } = require("stream");
 
-net.createServer(function (stream) {
-  stream
-      .pipe(crypto.createDecipher('aes192',  pw))
-      .pipe(net.connect(5000, 'localhost'))
-      .pipe(crypto.createCipher('aes192', pw))
-      .pipe(stream)
-}).listen(5005)
+const tunnel = new PassThrough();
+
+tunnel.on("data", (chunk) => {
+  console.log("bytes:", chunk);
+});
+
+const sharedSecret = crypto.randomBytes(32);
+const initializationVector = crypto.randomBytes(16);
+
+net
+  .createServer(function (stream) {
+    stream
+      .pipe(
+        crypto.createDecipheriv(
+          "aes-256-cbc",
+          sharedSecret,
+          initializationVector
+        )
+      )
+      .pipe(net.connect(5000, "localhost"))
+      .pipe(tunnel)
+      .pipe(
+        crypto.createCipheriv("aes-256-cbc", sharedSecret, initializationVector)
+      )
+      .pipe(stream);
+  })
+  .listen(5005, () => console.log("vpn server listening on port 5005"));
